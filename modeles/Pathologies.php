407209567995a46f9d4b790a377f275fbb6e8fbf
arca223 <arcanis223@gmail.com>
2015-03-09 16:39:03 +0100

    correction des fichiers

--- modeles/Pathologies.php
+++ modeles/Pathologies.php
@@ -1,44 +1,52 @@
 <?php
 
-require 'ConnexionBdd.php';
+Class Pathologies {
 
-global $bdd;
 
-function get_listePatho()
-{ 
-	$req = $bdd->prepare('SELECT nom_patho, type_patho, carac_patho FROM pathologies');
-	$req->execute();
-	$pathologies = $req->fetchAll();
-        
-	return $pathologies;
+    protected $_bdd;
+
+    public function __construct($bdd)
+    {
+        $this->_bdd = $bdd;
+    }
+
+    function get_listePatho()
+    {
+        $req = $bdd->prepare('SELECT nom_patho, type_patho, carac_patho FROM pathologies');
+        $req->execute();
+        $pathologies = $req->fetchAll();
+
+        return $pathologies;
+    }
+
+    function recherchePatho($nomPatho)
+    {
+        $req = $this->_bdd->prepare('SELECT nom_patho, type_patho, carac_patho FROM pathologies WHERE nom_patho = ?');
+        $req->bindParam($nomPatho);
+        $req->execute();
+        $patho = $req->fetch();
+
+        return $patho;
+    }
+
+    function ajoutPatho($nomPatho,$typePatho,$caracPatho)
+    {
+        $req = $this->_bdd->prepare('SELECT nom_patho, type_patho, carac_patho FROM pathologies WHERE nom_patho = ? AND type_patho = ? AND carac_patho = ?');
+        $req->bindParam(1, $nomPatho, PDO::PARAM_STR);
+        $req->bindParam(2, $typePatho, PDO::PARAM_STR);
+        $req->bindParam(3, $caracPatho, PDO::PARAM_STR);
+        $req->execute();
+        $ajout = $req->fetchAll();
+
+        if ($ajout['nom_patho'] == $nomPatho && $ajout['type_patho'] == $typePatho && $ajout['carac_patho'] == $caracPatho)
+        {
+            $req = $this->_bdd->prepare('INSERT INTO pathologies (nom_patho,type_patho,carac_patho) VALUES (?,?,?)');
+            $req->bindParam(1, $nomPatho, PDO::PARAM_STR);
+            $req->bindParam(2, $typePatho, PDO::PARAM_STR);
+            $req->bindParam(3, $caracPatho, PDO::PARAM_STR);
+            $req->execute();
+        }
+    }
+
 }
 
-function recherchePatho($nomPatho)
-{
-	$req = $bdd->prepare('SELECT nom_patho, type_patho, carac_patho FROM pathologies WHERE nom_patho = ?');
-	$req->bindParam($nom);
-	$nom = $nomPatho;
-	$req->execute();
-	$patho = $req->fetch();
-	
-	return $patho;
-}
-
-function ajoutPatho($nomPatho,$typePatho,$caracPatho)
-{
-	$req = $bdd->prepare('SELECT nom_patho, type_patho, carac_patho FROM pathologies WHERE nom_patho = ? AND type_patho = ? AND carac_patho = ?');
-	$req->bindParam(1, $nomPatho, PDO::PARAM_STR);
-	$req->bindParam(2, $typePatho, PDO::PARAM_STR);
-	$req->bindParam(3, $caracPatho, PDO::PARAM_STR);
-	$req->execute();
-	$ajout = $req->fetchAll();
-	
-	if ($ajout['nom_patho'] == $nomPatho && $ajout['type_patho'] == $typePatho && $ajout['carac_patho'] == $caracPatho)
-	{
-		$req = $bdd->prepare('INSERT INTO pathologies (nom_patho,type_patho,carac_patho) VALUES (?,?,?)');
-		$req->bindParam(1, $nomPatho, PDO::PARAM_STR);
-		$req->bindParam(2, $typePatho, PDO::PARAM_STR);
-		$req->bindParam(3, $caracPatho, PDO::PARAM_STR);
-		$req->execute();	
-	}
-}
--- modeles/listePatho.php
+++ modeles/listePatho.php
@@ -1,12 +1,24 @@
 <?php
-function get_listePatho()
-{
-    global $bdd;
-        
-    $req = $bdd->prepare('SELECT nom_patho, type_patho, carac_patho FROM pathologies');
-    $req->execute();
-    $pathologies = $req->fetchAll();
-        
-    return $pathologies;
-}
 
+Class ListePatho {
+
+
+    protected $_bdd;
+
+    public function __construct($bdd)
+    {
+        $this->_bdd = $bdd;
+    }
+
+
+    function get_listePatho()
+    {
+
+
+        $req = $this->_bdd->prepare('SELECT nom_patho, type_patho, carac_patho FROM pathologies');
+        $req->execute();
+        $pathologies = $req->fetchAll();
+
+        return $pathologies;
+    }
+}
