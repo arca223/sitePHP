a4ca90a00e62ee03e1eccea6b26eddaa90a14b7c
Ciols <loic.savoye@gmail.com>
2015-03-23 16:53:18 +0100

    footer

--- ressources/footer.php
+++ ressources/footer.php
@@ -1,4 +1,11 @@
 <?php
 
+echo '
+<a href="">Contact</a>
+<a href="http://validator.w3.org/">W3C Validator</a>
+<footer>
+	<img src="../banniere.jpg" alt="pied de page" width=100% height=300px>
+</footer>
+';
 
 echo 'Rien pour l\'instant';
