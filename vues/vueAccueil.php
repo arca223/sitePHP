e8218bc9b06e8bdbed5343b0e4cb990bc23f2726
arca223 <arcanis223@gmail.com>
2015-04-10 16:10:53 +0200

    xml

--- vues/vueAccueil.php
+++ vues/vueAccueil.php
@@ -3,11 +3,11 @@
 	$xslt = new XSLTProcessor();
 
 	$xslDoc = new DOMDocument();
-	$xslDoc->load('vueAccueil.xsl');
+	$xslDoc->load('C:\Users\Max TOMPOUCE\git\sitePHP\vues\vueAccueil.xsl');
 	$xslt->importStylesheet($xslDoc); 
 
 	$xmlDoc = new DOMDocument(); 
-	$xmlDoc->load('vueAccueil.xml');
+	$xmlDoc->load('C:\Users\Max TOMPOUCE\git\sitePHP\vues\vueAccueil.xml');
 	echo $xslt->transformToXML($xmlDoc);
 
 ?>
